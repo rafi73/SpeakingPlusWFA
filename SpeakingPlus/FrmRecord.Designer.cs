c81e0253020d42fce366cdc3f1623197cdf38290
Rahman35uap <45094930+Rahman35uap@users.noreply.github.com>
2018-11-19 18:02:28 +0900

    addition of volume meter and timer.Testing

--- SpeakingPlus/FrmRecord.Designer.cs
+++ SpeakingPlus/FrmRecord.Designer.cs
@@ -28,33 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRecord));
             this.btnRecord = new System.Windows.Forms.Button();
+            this.textBoxTimer = new System.Windows.Forms.TextBox();
+            this.pictureBoxVolumeMeterBack = new System.Windows.Forms.PictureBox();
+            this.pictureBoxVolumeMeterFront = new System.Windows.Forms.PictureBox();
+            this.timerVolumeMeter = new System.Windows.Forms.Timer(this.components);
+            this.timerForDisplay = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVolumeMeterBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVolumeMeterFront)).BeginInit();
             this.SuspendLayout();
             // 
             // btnRecord
             // 
-            this.btnRecord.Location = new System.Drawing.Point(99, 109);
+            this.btnRecord.Location = new System.Drawing.Point(80, 55);
             this.btnRecord.Name = "btnRecord";
-            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.Size = new System.Drawing.Size(109, 33);
             this.btnRecord.TabIndex = 0;
             this.btnRecord.Text = "Record";
             this.btnRecord.UseVisualStyleBackColor = true;
             this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // textBoxTimer
+            // 
+            this.textBoxTimer.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxTimer.Location = new System.Drawing.Point(80, 110);
+            this.textBoxTimer.Multiline = true;
+            this.textBoxTimer.Name = "textBoxTimer";
+            this.textBoxTimer.ReadOnly = true;
+            this.textBoxTimer.Size = new System.Drawing.Size(109, 37);
+            this.textBoxTimer.TabIndex = 1;
+            this.textBoxTimer.Text = "00:00:00";
+            // 
+            // pictureBoxVolumeMeterBack
+            // 
+            this.pictureBoxVolumeMeterBack.BackColor = System.Drawing.Color.Black;
+            this.pictureBoxVolumeMeterBack.Location = new System.Drawing.Point(70, 171);
+            this.pictureBoxVolumeMeterBack.Name = "pictureBoxVolumeMeterBack";
+            this.pictureBoxVolumeMeterBack.Size = new System.Drawing.Size(130, 40);
+            this.pictureBoxVolumeMeterBack.TabIndex = 2;
+            this.pictureBoxVolumeMeterBack.TabStop = false;
+            // 
+            // pictureBoxVolumeMeterFront
+            // 
+            this.pictureBoxVolumeMeterFront.BackColor = System.Drawing.Color.Green;
+            this.pictureBoxVolumeMeterFront.Location = new System.Drawing.Point(70, 171);
+            this.pictureBoxVolumeMeterFront.Name = "pictureBoxVolumeMeterFront";
+            this.pictureBoxVolumeMeterFront.Size = new System.Drawing.Size(10, 40);
+            this.pictureBoxVolumeMeterFront.TabIndex = 3;
+            this.pictureBoxVolumeMeterFront.TabStop = false;
+            // 
+            // timerVolumeMeter
+            // 
+            this.timerVolumeMeter.Enabled = true;
+            this.timerVolumeMeter.Tick += new System.EventHandler(this.timerVolumeMeter_Tick);
+            // 
+            // timerForDisplay
+            // 
+            this.timerForDisplay.Interval = 1000;
+            this.timerForDisplay.Tick += new System.EventHandler(this.timerForDisplay_Tick);
             // 
             // FrmRecord
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBoxVolumeMeterFront);
+            this.Controls.Add(this.pictureBoxVolumeMeterBack);
+            this.Controls.Add(this.textBoxTimer);
             this.Controls.Add(this.btnRecord);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "FrmRecord";
-            this.Text = "FrmRecord";
+            this.Text = "Speaking Plus";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVolumeMeterBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVolumeMeterFront)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.TextBox textBoxTimer;
+        private System.Windows.Forms.PictureBox pictureBoxVolumeMeterBack;
+        private System.Windows.Forms.PictureBox pictureBoxVolumeMeterFront;
+        private System.Windows.Forms.Timer timerVolumeMeter;
+        private System.Windows.Forms.Timer timerForDisplay;
+
     }
 }
--- SpeakingPlus/FrmRecord.cs
+++ SpeakingPlus/FrmRecord.cs
@@ -26,6 +26,14 @@ namespace SpeakingPlus
         //device is changed
         const int DBT_DEVNODES_CHANGED = 0x0007;
 
+        /* Audio Volume Meter */
+        private static Random rnd = new Random();
+        private static double audioValueMax = 0;
+        private static double audioValueLast = 0;
+        private static int audioCount = 0;
+        private static int RATE = 44100;
+        private static int BUFFER_SAMPLES = 1024;
+
         private const string APP_DIRECORY = "C:\\Users\\";
         private const string APP_PATH = "\\AppData\\Speaking-Plus\\";
         private string appPath = "";
@@ -36,12 +44,34 @@ namespace SpeakingPlus
         private WaveFileWriter waveFileWriterSoundCard;
         private WasapiLoopbackCapture wasApiLoopbackCapture;
 
+        int timerDuration = 0;
+        
+
         public FrmRecord()
         {
-            
             InitializeComponent();
+            volumeMeter();
+            timerVolumeMeter.Start();
         }
 
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            
+            if (recordMode)
+            {
+                timerForDisplay.Stop();
+                timerDuration = 0;
+                stopRecord();
+                
+            }
+            else
+            {
+                
+                createDirectory();
+                startRecord();
+                
+            }
+        }
         private void createDirectory()
         {
             appPath = APP_DIRECORY + Environment.UserName + APP_PATH + DateTime.Now.ToString("MM-dd-yyyy hh.mm.ss tt");
@@ -60,43 +90,45 @@ namespace SpeakingPlus
             Console.WriteLine(appPath);
         }
 
-        private void btnRecord_Click(object sender, EventArgs e)
-        {
-            if (recordMode)
-            {
-                stopRecord();
-            }
-            else
-            {
-                createDirectory();
-                startRecord();
-            }
-        }
-
         private void startRecord()
         {
 
-            if (checkMicrophone() && checkSpeaker())
+            if (checkSpeaker() && checkMicrophone())
             {
                 recordMode = true;
                 btnRecord.Text = "Stop";
 
-
-
-                recordFromMicrophone();
+                timerForDisplay.Start();
                 recordFromSoundCard();
+                recordFromMicrophone();
+                
+                
             }
         }
-
-        private void stopRecord()
+        private bool checkSpeaker()
         {
-            recordMode = false;
-            btnRecord.Text = "Record";
+            try
+            {
+                MMDeviceEnumerator mmDeviceEnumerator = new MMDeviceEnumerator();
+                MMDevice defaultDevice = mmDeviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Communications);
 
-            stopRecordMicrophone();
-            stopRecordFromSoundCard();
+                if (mmDeviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Communications).AudioEndpointVolume.Mute)
+                {
+                    MessageBox.Show("Your Default Speaker is in Mute position.Please Unmute it.");
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                if (exception.HResult == -2147023728)
+                {
+                    MessageBox.Show("No audio output device found");
+                }
+                return false;
+            }
         }
-
         private bool checkMicrophone()
         {
             try
@@ -126,71 +158,13 @@ namespace SpeakingPlus
                 return false;
             }
         }
-
-        private bool checkSpeaker()
-        {
-            try
-            {
-                MMDeviceEnumerator mmDeviceEnumerator = new MMDeviceEnumerator();
-                MMDevice defaultDevice = mmDeviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Communications);
-
-                if (mmDeviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Communications).AudioEndpointVolume.Mute)
-                {
-                    MessageBox.Show("Your Default Speaker is in Mute position.Please Unmute it.");
-                    return false;
-                }
-                return true;
-            }
-            catch (Exception exception)
-            {
-                if (exception.HResult == -2147023728)
-                {
-                    MessageBox.Show("No audio output device found");
-                }
-                return false;
-            }
-        }
-
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == WM_DEVICECHANGE)
-            {
-                if(recordMode)
-                {
-                    stopRecordFromSoundCard();
-                    stopRecordMicrophone();
-                    recordMode = false;
-                }
-                MessageBox.Show("Microphone is Unplugged, Recoeding has been stopped!");
-            }
-            base.WndProc(ref m);
-        }
-
-        private void recordFromMicrophone()
-        {
-            int deviceNumber = 0;
-            waveInMicrophone = new WaveIn();
-            waveInMicrophone.DeviceNumber = deviceNumber;
-            waveInMicrophone.WaveFormat = new WaveFormat(44100, WaveIn.GetCapabilities(deviceNumber).Channels);
-            waveInMicrophone.DataAvailable += new EventHandler<WaveInEventArgs>(microphoneFileWriter);
-            waveFileWriterMicrophone = new WaveFileWriter(appPath + "\\mic.wav", waveInMicrophone.WaveFormat);
-            waveInMicrophone.StartRecording();
-        }
-
-        private void microphoneFileWriter(object sender, WaveInEventArgs e)
-        {
-            if (waveFileWriterMicrophone == null) return;
-            waveFileWriterMicrophone.Write(e.Buffer, 0, e.BytesRecorded);
-            waveFileWriterMicrophone.Flush();
-        }
-
         private void recordFromSoundCard()
         {
             string outputFilePath = appPath + @"\scr.wav";
             wasApiLoopbackCapture = new WasapiLoopbackCapture();
-            waveFileWriterSoundCard = new WaveFileWriter(outputFilePath, wasApiLoopbackCapture.WaveFormat); 
+            waveFileWriterSoundCard = new WaveFileWriter(outputFilePath, wasApiLoopbackCapture.WaveFormat);
 
-            wasApiLoopbackCapture.DataAvailable += (s, a) => 
+            wasApiLoopbackCapture.DataAvailable += (s, a) =>
             {
                 waveFileWriterSoundCard.Write(a.Buffer, 0, a.BytesRecorded);
             };
@@ -202,24 +176,50 @@ namespace SpeakingPlus
                 wasApiLoopbackCapture.Dispose();
             };
             wasApiLoopbackCapture.StartRecording();
-        }
+            
 
+        }
+        private void recordFromMicrophone()
+        {
+            int deviceNumber = 0;
+            waveInMicrophone = new WaveIn();
+            waveInMicrophone.DeviceNumber = deviceNumber;
+            waveInMicrophone.WaveFormat = new WaveFormat(44100, WaveIn.GetCapabilities(deviceNumber).Channels);
+            waveInMicrophone.DataAvailable += new EventHandler<WaveInEventArgs>(microphoneFileWriter);
+            waveFileWriterMicrophone = new WaveFileWriter(appPath + "\\mic.wav", waveInMicrophone.WaveFormat);
+            waveInMicrophone.StartRecording();
+
+        }
+        private void microphoneFileWriter(object sender, WaveInEventArgs e)
+        {
+            if (waveFileWriterMicrophone == null) return;
+            waveFileWriterMicrophone.Write(e.Buffer, 0, e.BytesRecorded);
+            waveFileWriterMicrophone.Flush();
+        }
+        private void stopRecord()
+        {
+            recordMode = false;
+            btnRecord.Text = "Record";
+            stopRecordFromSoundCard();
+            stopRecordMicrophone();
+            
+            
+        }
         private void stopRecordFromSoundCard()
         {
             wasApiLoopbackCapture.StopRecording();
             waveFileWriterSoundCard.Dispose();
             convertToMonoChannelAudio(appPath + "\\scr.wav", appPath + "\\scr-mono.wav");
+            
         }
-        
         private void stopRecordMicrophone()
         {
-            waveInMicrophone.StopRecording();   
+            waveInMicrophone.StopRecording();
             waveInMicrophone.Dispose();
             waveFileWriterMicrophone.Dispose();
 
             convertToMonoChannelAudio(appPath + "\\mic.wav", appPath + "\\mic-mono.wav");
         }
-
         private void convertToMonoChannelAudio(string dualChanelAudio, string monoChannelAudio)
         {
             using (var inputReader = new AudioFileReader(dualChanelAudio))
@@ -232,5 +232,102 @@ namespace SpeakingPlus
             }
             File.Delete(dualChanelAudio);
         }
+        
+        
+        private void MicrophoneVolumeLevel(object sender, WaveInEventArgs args)
+        {
+
+            float max = 0;
+
+            // interpret as 16 bit audio
+            for (int index = 0; index < args.BytesRecorded; index += 2)
+            {
+                short sample = (short)((args.Buffer[index + 1] << 8) |
+                                        args.Buffer[index + 0]);
+                var sample32 = sample / 32768f; // to floating point
+                if (sample32 < 0) sample32 = -sample32; // absolute value 
+                if (sample32 > max) max = sample32; // is this the max value?
+            }
+
+            // calculate what fraction this peak is of previous peaks
+            if (max > audioValueMax)
+            {
+                audioValueMax = (double)max;
+            }
+            audioValueLast = max;
+            audioCount += 1;
+        }
+        private void volumeMeter()
+        {
+            WaveInEvent waveIn = new WaveInEvent();
+            waveIn.DeviceNumber = 0;
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(RATE, 1);
+            waveIn.DataAvailable += MicrophoneVolumeLevel;
+            waveIn.BufferMilliseconds = (int)((double)BUFFER_SAMPLES / (double)RATE * 1000.0);
+            waveIn.StartRecording();
+
+        }
+        
+
+        private void timerVolumeMeter_Tick(object sender, EventArgs e)
+        {
+            double percentageOfVolume = audioValueLast / audioValueMax;
+            pictureBoxVolumeMeterFront.Width = (int)(percentageOfVolume * pictureBoxVolumeMeterBack.Width);
+        }
+        
+        private void timerForDisplay_Tick(object sender, EventArgs e)
+        {
+            timerDuration = timerDuration + 1;
+            string time = "00:00:00";
+            int hh = (int)timerDuration / 3600;
+            int mm = (int)((timerDuration % 3600) / 60);
+            int ss = (timerDuration % 3600) % 60;
+            if (hh < 10)
+            {
+                time = "0" + hh.ToString();
+            }
+            else
+            {
+                time = hh.ToString();
+            }
+            time = time + ":";
+            if (mm < 10)
+            {
+                time = time + "0" + mm.ToString();
+            }
+            else
+            {
+                time = time + mm.ToString();
+            }
+            time = time + ":";
+            if (ss < 10)
+            {
+                time = time + "0" + ss.ToString();
+            }
+            else
+            {
+                time = time + ss.ToString();
+            }
+            textBoxTimer.Text = time;
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_DEVICECHANGE)
+            {
+                if (recordMode)
+                {
+                    stopRecord();
+                    timerForDisplay.Stop();
+                    timerDuration = 0;
+                    
+
+                }             
+                MessageBox.Show("Microphone is Unplugged, Recording has been stopped!");
+                volumeMeter();
+            }
+            
+            base.WndProc(ref m);
+        }
+
     }
 }
